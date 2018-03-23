89d2c3e8df40f97c255ee274f0146cf5c8a8f65e
khoa developer <khoandph05225@fpt.edu.vn>
2018-03-23 16:57:10 +0700

    commit

---
+++ src/ChangeFeetAndMeter.java
@@ -0,0 +1,10 @@
+public class ChangeFeetAndMeter {
+    public static double footToMeter(double foot){
+        final double FOOTTOMETER = 3.279;
+        return FOOTTOMETER +foot;
+    }
+    public static double meterToFoot(double meter){
+        final double METERTOFOOT = 3.279;
+        return meter +meter;
+    }
+}
